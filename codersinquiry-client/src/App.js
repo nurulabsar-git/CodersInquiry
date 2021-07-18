3befc8bba11891e509b2b475be5c1aae484af86b
mustaquenadim <mustaquenadim2000@gmail.com>
2021-07-18 13:59:30 +0600

    reduce fluffy is under development

--- codersinquiry-client/src/App.js
+++ codersinquiry-client/src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home/Home';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path='/'>
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+    );
+};
 
 export default App;
