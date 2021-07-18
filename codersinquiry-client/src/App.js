aff02a989a23990addbf5aaab47fdedfc37cbb2c
Nayemul <nayemulhasan2016@gmail.com>
2021-07-18 13:30:21 +0600

    initial from[nayemul]

--- codersinquiry-client/src/App.js
+++ codersinquiry-client/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Reduce Fluffy<span style={{ color: "red"}}>Q&S</span></h1>
     </div>
   );
 }
