5db47d98324d28380e99b4d1b45d988373e88362
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-07-19 17:07:30 +0600

    update

--- codersinquiry-client/src/components/Home/Home.jsx
+++ codersinquiry-client/src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ import construction from '../../images/construction.svg';
 const Home = () => {
     return (
         <div className='home text-center'>
-            <img src={construction} alt="" />
+            <img src={construction} alt=""  style={{width: '100%'}}/>
             <h1>Reduce Fluffy</h1>
             <h2>This site is under development.</h2>
         </div>
