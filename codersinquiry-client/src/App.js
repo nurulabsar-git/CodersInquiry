0979dd1b4acbc946c59c50b2c8cd1c6fd667bc61
mustaquenadim <mustaquenadim2000@gmail.com>
2021-07-19 20:08:45 +0600

    working on Header

--- codersinquiry-client/src/App.js
+++ codersinquiry-client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './components/Home/Home';
+import Header from './components/Header/Header';
 
 
 const App = () => {
     return (
         <Router>
+            <Header />
             <Switch>
                 <Route exact path='/'>
                     <Home />
--- codersinquiry-client/src/components/Home/Home.jsx
+++ codersinquiry-client/src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ import construction from '../../images/construction.svg';
 const Home = () => {
     return (
         <div className='home text-center'>
-            <img src={construction} alt="" />
+            <img src={construction} alt="" className='home__img' />
             <h1>Reduce Fluffy</h1>
             <h2>This site is under development.</h2>
         </div>
