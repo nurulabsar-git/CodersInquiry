42368b28914826ce22e1d59067f53ffa66fad968
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-07-24 10:26:03 +0600

    update

---
+++ codersinquiry-client/src/components/CustomNavbar/CustomNavbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CustomNavbar = () => {
+    return (
+        <div>
+            <h4>This is custom navbar</h4>
+        </div>
+    );
+};
+
+export default CustomNavbar;
