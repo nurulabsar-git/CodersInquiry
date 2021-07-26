3f5329513bfa7821b728c9cf0357371b2a8e3f85
Nayemul <nayemulhasan2016@gmail.com>
2021-07-26 20:18:22 +0600

    add animation gif[nayemul]

--- codersinquiry-client/src/components/Banner/Banner.jsx
+++ codersinquiry-client/src/components/Banner/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
                     <h1>প্রোগ্রামিং সম্পর্কিত যেকোন সমস্যার জন্য প্রশ্ন করুন</h1>
                 </div>
                 <div className='col'>
-                    <img src="" alt="" />
+                    <img height="100px" width="400px" src="http://2.bp.blogspot.com/-BbKTOFDGquw/XE0rqJY2LDI/AAAAAAAADaI/8q-Q8T9LvY0zULgL2VKMjG8xT7DaBO3ZwCK4BGAYYCw/s1600/wow.gif" alt="" />
                 </div>
             </div>
         </div>
