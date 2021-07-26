90a026e71d0c7fa9fb5628a058ef8302e6c16b61
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-07-26 23:26:48 +0600

    Absar

--- codersinquiry-client/src/components/Navbar/Navbar.jsx
+++ codersinquiry-client/src/components/Navbar/Navbar.jsx
@@ -5,13 +5,13 @@ import Login from '../Login/Login';
 
 const Navbar = () => {
     return (
-        <nav class='navbar navbar-expand-lg navbar-light bg-light'>
+        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
             <div class='container-fluid'>
                 <Link class='navbar-brand' to='/'>
                     Reduce Fluffy
                 </Link>
-                <form class='d-flex'>
-                    <input class='form-control me-2' type='search' placeholder='Search' aria-label='Search' />
+                <form className='d-flex'>
+                    <input className='form-control me-2' type='search' placeholder='Search' aria-label='Search' />
                     <button class='btn btn-outline-success' type='submit'>
                         Search
                     </button>
