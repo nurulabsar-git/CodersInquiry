09d4adc84138a9679110d28fbaead67d3471f47e
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-07-27 00:56:05 +0600

    update

--- codersinquiry-client/src/components/Header/Header.jsx
+++ codersinquiry-client/src/components/Header/Header.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './Header.css';
 import Navbar from '../Navbar/Navbar';
 import Banner from '../Banner/Banner';
-
+import CustomNavbar from '../CustomNavbar/CustomNavbar'
 const Header = () => {
+    var style = {
+        width: '300px'
+    } 
     return (
         <div className='header'>
-            <Navbar />
+               { style ? <CustomNavbar/> :<Navbar/> }
             <Banner />
         </div>
     );
