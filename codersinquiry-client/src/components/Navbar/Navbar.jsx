f80e82cef40269dfbfeb00ca94693f1a2dbc62b4
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-07-27 11:38:14 +0600

    absar

--- codersinquiry-client/src/components/Navbar/Navbar.jsx
+++ codersinquiry-client/src/components/Navbar/Navbar.jsx
@@ -39,3 +39,43 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+// import React from 'react';
+// import logo from '../../images/js.png'
+// import { Container } from 'react-bootstrap';
+// const blogData = [
+//     {
+//      title: 'What is a cors policy?',
+//      logo: logo,
+//     },
+//     {
+//         title: 'What is a cors policy?',
+//         logo: logo,
+//        },
+//        {
+//         title: 'What is a cors policy? Cors policy..',
+//         logo: logo,
+//        },
+
+// ]
+// const Blog = () => {
+//     const styles ={display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', padding: '10px'}
+
+//     return (
+//         <Container>
+//             This is blogging section
+//             <div style={styles}>
+//              {   blogData.map((datum) => ( 
+               
+//                  <div className="d-flex justify-content-around m-2">
+//                     <img src={datum.logo} alt="" style={{width: '150px'}}/>
+//                  </div>
+               
+                
+//                 ))}
+//             </div>
+//         </Container>
+//     );
+// };
+
+// export default Blog;
