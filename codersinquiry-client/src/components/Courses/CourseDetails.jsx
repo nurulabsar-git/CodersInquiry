07228dbc2003112e19ca5bbfe14145831ca32de0
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-07-28 12:40:48 +0600

    absar

--- codersinquiry-client/src/components/Courses/CourseDetails.jsx
+++ codersinquiry-client/src/components/Courses/CourseDetails.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 
 const CourseDetails = () => {
   const {courseId} = useParams();
 
+   const style= {
+       textAlign: 'center',
+       padding: '50px',
+    }
     return (
-        <div>
-          <h4>This is course {courseId} details page</h4>  
-        </div>
+        <Container>
+          <h4 style={style}>This is course {courseId} details page</h4>  
+        </Container>
     );
 };
 
