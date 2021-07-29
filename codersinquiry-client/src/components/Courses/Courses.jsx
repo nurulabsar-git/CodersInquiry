edb0be7974a199939110fc098649a119debae3d6
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-07-29 21:33:38 +0600

    absar

--- codersinquiry-client/src/components/Courses/Courses.jsx
+++ codersinquiry-client/src/components/Courses/Courses.jsx
@@ -63,6 +63,7 @@ const Courses = () => {
     const history = useHistory();
     const cardStyle = {boxShadow: 'rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset', justifyContent: 'space-around',  border: '1.8px solid #dce2e5', borderRadius: '4px', textAlign: 'center', height: '196px', width: '174px', marginBottom: '10px', marginRight: '10px'}
     
+
     const handleCourseDetails = (parameter) => {
        history.push(`/courseDetails/${parameter}`)
     }
