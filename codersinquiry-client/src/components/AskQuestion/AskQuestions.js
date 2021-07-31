fe5d9691f77b37f7935a2981f4f647b35d346586
unknown <monsuralam7262@gmail.com>
2021-08-01 02:49:30 +0600

    ask question & write article [monsur]

--- codersinquiry-client/src/components/AskQuestion/AskQuestions.js
+++ codersinquiry-client/src/components/AskQuestion/AskQuestions.js
@@ -1,39 +1,40 @@
 import React from 'react';
-import './AskQuestions.css';
 import { useForm } from 'react-hook-form';
+import Footer from '../Footer/Footer';
 import Navbar from '../Navbar/Navbar';
+import thinker from '../../images/thinker.png';
+import './AskQuestions.css';
 
 const AskQuestions = () => {
-
     const { register, handleSubmit, formState: { errors } } = useForm();
-
     const onSubmit = data => {
         console.log(data);
     }
 
     return (
         <div>
-            <Navbar></Navbar>
-            <section>
-                <div className="form-div row d-flex justify-content-center">
-                    <div className="col-md-6">
-                        <form onSubmit={handleSubmit(onSubmit)} className="p-5">
-                            <input className="form-control mb-3" type="text" placeholder="Enter Title" {...register("title", { required: true })} />
+            <Navbar />
+            <div className="d-flex justify-content-around form_section">
+                <form onSubmit={handleSubmit(onSubmit)} className="p-5 form">
+                    <p>টাইটেল</p>
+                    <input className="form-control mb-1" type="text" placeholder="টাইটেল লিখুন..." {...register("title", { required: true })} />
+ 
+                    {errors.title && errors.title.type === "required" && <span style={{ color: "coral" }}>টাইটেল আবশ্যক</span>}
+                    <p>প্রশ্ন/সমস্যা</p>
+                    <textarea className="form-control mb-1" placeholder="আপনার প্রশ্ন/সমস্যা লিখুন..." {...register("description", { required: true })} />
 
-                            {errors.title && errors.title.type === "required" && <span>Title is required</span>}
+                    {errors.description && errors.description.type === "required" && <span style={{ color: "coral" }}>প্রশ্ন/সমস্যা'র অংশ আবশ্যক</span>}
 
-                            <textarea className="form-control mb-3" placeholder="Type Your Question" {...register("description", { required: true })} />
-
-                            {errors.description && errors.description.type === "required" && <span>Description is required</span>}
-
-                            {/* <input onChange={handleImageUpload} type="file" className="form-control mb-3" /> */}
-                            <input type="file" className="form-control mb-3" />
-
-                            <input className="btn btn-primary text-white py-2 px-3" type="submit" value="submit" />
-                        </form>
-                    </div>
-                </div>
-            </section>
+                    {/* <input onChange={handleImageUpload} type="file" className="form-control mb-3" /> */}
+                    <p>স্ক্রিনশট/ছবি</p>
+                    <input type="file" className="form-control mb-3" />
+ 
+                    <input className="btn btn-warning text-black py-2 px-3" type="submit" value="সাবমিট করুন" />
+                </form>
+                <img src={thinker} alt={thinker} />
+            </div>
+        
+            <Footer />
         </div>
     );
 };
