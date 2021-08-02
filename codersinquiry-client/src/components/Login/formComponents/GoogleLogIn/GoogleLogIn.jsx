d9b1c19824145f0bf50371ce34f402dcd175fc37
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-08-02 14:34:19 +0600

    absar

---
+++ codersinquiry-client/src/components/Login/formComponents/GoogleLogIn/GoogleLogIn.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { useState } from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './Firebase.config';
+
+const GoogleLogIn = () => {
+   
+    const [loggedInUser, setLoggedInUser] = useState();
+    if(firebase.apps.length === 0){
+        firebase.initializeApp(firebaseConfig);
+        }
+
+
+
+    const handleGoogleSignIn = () => {
+        var provider = new firebase.auth.GoogleAuthProvider();
+           firebase.auth()
+           .signInWithPopup(provider)
+           .then((result) => {
+          
+           const {displayName, email} = result.user;
+           const signInUser = {name: displayName, email}
+           var credential = result.credential;
+           var token = credential.accessToken;
+           var user = result.user;
+           setLoggedInUser(signInUser);
+     })
+     .catch((error) => {
+      
+       var errorCode = error.code;
+       var errorMessage = error.message;
+      var email = error.email;
+      var credential = error.credential;
+      
+     });
+       }
+
+
+    return (
+        <div>
+            <button onClick={handleGoogleSignIn}><i class="fa fa-google" aria-hidden="true"></i>Google </button>
+        </div>
+    );
+};
+
+export default GoogleLogIn;
