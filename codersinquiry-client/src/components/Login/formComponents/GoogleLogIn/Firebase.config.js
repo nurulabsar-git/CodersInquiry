4967c6b32a3684c21d7af873d1c73210396b05f2
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-08-04 01:59:03 +0600

    absar

---
+++ codersinquiry-client/src/components/Login/formComponents/GoogleLogIn/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD1JgUJmivPU1du_vmkX4D7pyyEnfP0AkE",
+    authDomain: "reducefluffy.firebaseapp.com",
+    projectId: "reducefluffy",
+    storageBucket: "reducefluffy.appspot.com",
+    messagingSenderId: "393696518264",
+    appId: "1:393696518264:web:695d0ecec39bb4cdc10d8d"
+  };
+
+  export default firebaseConfig;
