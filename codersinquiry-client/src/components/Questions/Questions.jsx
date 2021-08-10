ec8dc508571f2038d2e0d84aed235411d4cad6ae
unknown <monsuralam7262@gmail.com>
2021-08-10 19:16:33 +0600

    update

--- codersinquiry-client/src/components/Questions/Questions.jsx
+++ codersinquiry-client/src/components/Questions/Questions.jsx
@@ -16,6 +16,7 @@ const Questions = () => {
             <div className='questions__filter'>
                 <ul class='nav nav-pills mb-3' id='pills-tab' role='tablist'>
                     <li class='nav-item' role='presentation'>
+
                         <button class='nav-link active' id='pills-home-tab' data-bs-toggle='pill' data-bs-target='#pills-home' type='button' role='tab' aria-controls='pills-home' aria-selected='true'>
                             Home
                         </button>
